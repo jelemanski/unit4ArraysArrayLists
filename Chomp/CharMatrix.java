c80bf4a47e69254574a6e0aa586cf9bbc73a4b6b
jelemanski <jelemanski98@gmail.com>
2015-12-07 10:29:16 -0600

    more practice

--- Chomp/CharMatrix.java
+++ Chomp/CharMatrix.java
@@ -9,14 +9,14 @@ public class CharMatrix
     public CharMatrix(int rows, int cols)
     {
         char[][] grid = new char[rows][cols];
-        
-         for(int i=0; i<rows ;i++)
+
+        for(int i=0; i<rows ;i++)
         {
             for(int e = 0; e<cols;e++)
             {
                 grid[i][e] = ' ';
             }
-            
+
         }
 
     }
@@ -31,7 +31,7 @@ public class CharMatrix
             {
                 grid[i][e] = fill;
             }
-            
+
         }
     }
 
@@ -56,14 +56,14 @@ public class CharMatrix
     // Sets the character at row, col location to ch
     public void setCharAt(int row, int col, char ch)
     {
-        char ch = grid[row][cols];
+        ch = grid[row][col];
     }
 
     // Returns true if the character at row, col is a space,
     // false otherwise
     public boolean isEmpty(int row, int col)
     {
-        if (grid[row][col] = ' ')
+        if (grid[row][col] == ' ')
         { 
             return true;
         }
@@ -78,26 +78,52 @@ public class CharMatrix
     // lower right corner of the rectangle.
     public void fillRect(int row0, int col0, int row1, int col1, char fill)
     {
-        ...
-    }
+        for( int i=row0; i<=row1; i++)
+        {
+            for(int e=col0; e<=col1; e++)
+            {
 
+                grid[i][e] = fill;
+
+            }
+        }
+
+    }
     // Fills the given rectangle with SPACE characters.
     // row0, col0 is the upper left corner and row1, col1 is the
     // lower right corner of the rectangle.
     public void clearRect(int row0, int col0, int row1, int col1)
     {
-        ...
+        for(int i=row0; i<=row1; i++)
+        {
+            for(int e=col0; e<=col1; e++)
+            {
+
+                grid[i][e] = ' ';
+
+            }
+        }
+        
     }
 
     // Returns the count of all non-space characters in row 
     public int countInRow(int row)
     {
-        ...
+        int e = 0;
+       for(int i = 0; i<=row;i++)
+       {
+           if (row[i] == ' ')
+           { e++
+            }
+        }
     }
 
     // Returns the count of all non-space characters in col 
     public int countInCol(int col)
     {
-        ...
+         for(int i=0; i<=col.length();i++)
+       {
+           return i;
+    }
     }
 }
